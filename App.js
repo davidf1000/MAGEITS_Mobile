d26cc91a6b21650cc21d4ad3b6523f28241feb7e
davidf1000 <47879766+davidf1000@users.noreply.github.com>
2020-10-10 16:33:45 +0700

    init

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [val,setVal]= useState(0);
+  const clickButton = ()=>{
+    setVal(val+1);
+  } 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{val}</Text>
+      <Button title="Change Text !" onPress={clickButton} />
       <StatusBar style="auto" />
     </View>
   );
